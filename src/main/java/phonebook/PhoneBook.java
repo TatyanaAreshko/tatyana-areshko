c21c27d687cb2f031ff316e07599b61a44404535
TASJA <tatyana.areshko@gmail.com>
2021-06-09 15:20:57 +0300

    Phonebook

---
+++ src/main/java/phonebook/PhoneBook.java
@@ -0,0 +1,38 @@
+package phonebook;
+
+import java.util.*;
+
+public class PhoneBook {
+
+    private static final Map<String, Set<String>> phones = new HashMap<>();
+
+    public static Map<String, Set<String>> getPhones() {
+        return phones;
+    }
+
+    public void add(String name, String... number) {
+        //присваиваем значение поля имя переменной numbers
+        Set<String> numbers = new HashSet<>(Arrays.asList(number));
+        phones.put(name, numbers);
+    }
+
+    //создаем список имен сотрудников
+    public Set<String> get(String name) {
+        return phones.get(name);
+    }
+
+    // проверка
+   /* public void check(String name, String number) {
+        List<String> num = new ArrayList<>();
+        if (this.phones.containsKey(name)) {
+            num = this.phones.get(name);
+            num.add(name);
+            this.phones.put(name, phones.get(number));
+        } else {
+            num.add(name);
+            this.phones.put(name, phones.get(number));
+        }
+        System.out.println(phones);
+
+    }*/
+}
---
+++ src/main/java/phonebook/Runner.java
@@ -0,0 +1,36 @@
+package phonebook;
+
+
+import java.util.*;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Runner extends PhoneBook{
+
+    public static void main(String[] args) {
+
+        PhoneBook phoneBook = new PhoneBook();
+
+        //Map<String, String> info = new HashMap<String, String>();
+
+        phoneBook.add("Ivan", "123", "555");
+
+        phoneBook.add("Petr", "456");
+        phoneBook.add("Max", "777");
+        phoneBook.add("Ivan", "123");
+
+
+        System.out.println(getPhones());
+        System.out.println("___________________");
+        System.out.println(phoneBook.get("Max"));
+
+
+
+
+
+
+
+
+    }
+}
